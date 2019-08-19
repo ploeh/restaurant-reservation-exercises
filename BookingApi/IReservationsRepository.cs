f6054466a2b797358a065f08536c195eb1bd09fc
Mark Seemann <mark@ploeh.dk>
2019-08-19 12:30:28 +0200

    Implement happy path for TryAccept
    From a strict TDD perspective, a much simpler implementation the method
    is possible: just return 42.
    
    The purpose of this code base, however, is to teach topics like
    interaction-based versus state-based testing, and only later in the
    course will we introduce parametrised tests and property-based testing.

---
+++ BookingApi/IReservationsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ploeh.Samples.BookingApi
+{
+    public interface IReservationsRepository
+    {
+        IEnumerable<Reservation> ReadReservations(DateTime date);
+        int Create(Reservation reservation);
+    }
+}
