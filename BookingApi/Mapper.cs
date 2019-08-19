074329e56e32377426f1f6439f8dd51de574551c
Mark Seemann <mark@ploeh.dk>
2019-08-19 15:52:41 +0200

    Add Mapper

---
+++ BookingApi/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.BookingApi
+{
+    public class Mapper : IMapper
+    {
+        public Reservation Map(ReservationDto dto)
+        {
+            return new Reservation
+            {
+                Date = DateTime.Parse(dto.Date),
+                Email = dto.Email,
+                Name = dto.Name,
+                Quantity = dto.Quantity
+            };
+        }
+    }
+}
