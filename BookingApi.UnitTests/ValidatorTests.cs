4ce28980ed89fd1f7b16732c26590497b226d6c7
Mark Seemann <mark@ploeh.dk>
2019-08-19 15:26:48 +0200

    Implement Validator

---
+++ BookingApi.UnitTests/ValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.Samples.BookingApi.UnitTests
+{
+    public class ValidatorTests
+    {
+        [Fact]
+        public void ValidDate()
+        {
+            var dto = new ReservationDto
+            {
+                Date = "2018-08-30T19:47:00"
+            };
+            var sut = new Validator();
+
+            var actual = sut.Validate(dto);
+
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void InvalidDate()
+        {
+            var dto = new ReservationDto
+            {
+                Date = "Invalid date"
+            };
+            var sut = new Validator();
+
+            var actual = sut.Validate(dto);
+
+            Assert.NotEmpty(actual);
+        }
+    }
+}
