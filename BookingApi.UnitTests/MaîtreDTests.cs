b0098749f2c6b853da4d759637e9b8cc0172944e
Mark Seemann <mark@ploeh.dk>
2019-08-22 11:11:51 +0200

    Parametrise test with ClassData
    This is an example of how the underlying [Theory] mechanism works. If
    you can somehow supply an IEnumerable<object[]>, you can provide data to
    a [Theory].
    
    The inner object array contains the arguments for a single test case,
    while the IEnumerable provides multiple argument lists; in other words:
    multiple test cases.

--- BookingApi.UnitTests/MaîtreDTests.cs
+++ BookingApi.UnitTests/MaîtreDTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,19 +8,31 @@ namespace Ploeh.Samples.BookingApi.UnitTests
 {
     public class MaîtreDTests
     {
-        [Theory]
-        [InlineData("2018-08-30",  4, 10)]
-        [InlineData("2019-09-29", 10, 10)]
-        [InlineData("2020-10-28", 20, 20)]
-        [InlineData("2021-11-27",  1, 22)]
+        private class HappyPath : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { new DateTime(2018,  8, 30),  4, 10 };
+                yield return new object[] { new DateTime(2019,  9, 29), 10, 10 };
+                yield return new object[] { new DateTime(2020, 10, 28), 20, 20 };
+                yield return new object[] { new DateTime(2021, 11, 27),  1, 22 };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return this.GetEnumerator();
+            }
+        }
+
+        [Theory, ClassData(typeof(HappyPath))]
         public void CanAcceptReturnsReservationInHappyPathScenario(
-            string date,
+            DateTime date,
             int quantity,
             int capacity)
         {
             var reservation = new Reservation
             {
-                Date = DateTime.Parse(date),
+                Date = date,
                 Quantity = quantity
             };
             var sut = new MaîtreD(capacity);
