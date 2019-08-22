6f0784edefd10496940e6d79c4335c1f119f5dce
Mark Seemann <mark@ploeh.dk>
2019-08-22 14:19:18 +0200

    Refactor parametrised test 2 take whole object arg
    This demonstrates that once you make the transition from [InlineData] to
    something like [ClassData], you're no longer constrained to 'simple'
    arguments.

--- BookingApi.UnitTests/MaîtreDTests.cs
+++ BookingApi.UnitTests/MaîtreDTests.cs
@@ -8,38 +8,30 @@ namespace Ploeh.Samples.BookingApi.UnitTests
 {
     public class MaîtreDTests
     {
-        private class HappyPath : TheoryData<DateTime, int, int>
+        private class HappyPath : TheoryData<Reservation, int>
         {
             public HappyPath()
             {
-                Add(new DateTime(2018,  8, 30),  4, 10);
-                Add(new DateTime(2019,  9, 29), 10, 10);
-                Add(new DateTime(2020, 10, 28), 20, 20);
-                Add(new DateTime(2021, 11, 27),  1, 22);
+                Add(new Reservation { Date = new DateTime(2018,  8, 30), Quantity =  4 }, 10);
+                Add(new Reservation { Date = new DateTime(2019,  9, 29), Quantity = 10 }, 10);
+                Add(new Reservation { Date = new DateTime(2020, 10, 28), Quantity = 20 }, 20);
+                Add(new Reservation { Date = new DateTime(2021, 11, 27), Quantity =  1 }, 22);
             }
         }
 
         [Theory, ClassData(typeof(HappyPath))]
         public void CanAcceptReturnsReservationInHappyPathScenario(
-            DateTime date,
-            int quantity,
+            Reservation reservation,
             int capacity)
         {
-            var reservation = new Reservation
-            {
-                Date = date,
-                Quantity = quantity
-            };
             var sut = new MaîtreD(capacity);
-
             var actual = sut.CanAccept(new Reservation[0], reservation);
-
             Assert.True(actual);
         }
 
         [Theory]
-        [InlineData( 4, 10)]
-        [InlineData( 3,  3)]
+        [InlineData(4, 10)]
+        [InlineData(3, 3)]
         [InlineData(11, 14)]
         public void CanAcceptOnInsufficientCapacity(int quantity, int capacity)
         {
